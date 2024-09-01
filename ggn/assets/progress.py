e663f2b145c0a903a4da9543e526c7cda007f9e8
Rizz <123063244+white-wolf-mia@users.noreply.github.com>
2024-09-01 22:13:08 +0530

    Update progress.py

--- ggn/assets/progress.py
+++ ggn/assets/progress.py
@@ -5,8 +5,8 @@ import json
 from ggn.assets.functions import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
-UN_FINISHED_PROGRESS_STR = "🔴"
+FINISHED_PROGRESS_STR = "≈≈"
+UN_FINISHED_PROGRESS_STR = "≉≉"
 DOWNLOAD_LOCATION = "/app"
 
 
